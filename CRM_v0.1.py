94439aca067d91b3ae3f13dcb1d9148ac88968f7
actsolucoesparapessoas <129021588+actsolucoesparapessoas@users.noreply.github.com>
2024-03-25 22:36:34 -0300

    Update CRM_v0.1.py

--- CRM_v0.1.py
+++ CRM_v0.1.py
@@ -222,7 +222,7 @@ def Exibir_Abas():
                         {"id": 5, "content": "Acontecimento 05", "start": "2022-10-21"}]          
             
             st.subheader("Linha do tempo:")
-            timeline(items, height=800) 
+            st.write(timeline(items, height=800) )
             #tline = timeline(items, height=800)          
             #st.write(tline)            
             st.divider()
