a65efce4f8a817c67c0dfb0ebd9d192c7053cddb
actsolucoesparapessoas <129021588+actsolucoesparapessoas@users.noreply.github.com>
2024-03-28 16:40:46 -0300

    Update gemini01.py

--- gemini01.py
+++ gemini01.py
@@ -5,6 +5,7 @@ $ pip install google-generativeai
 """
 import streamlit as st
 import google.generativeai as genai
+from gtts import gTTS
 
 genai.configure(api_key="AIzaSyChCrbdYRD0AVTVf3RzFkxw2XzKooATnMQ")
 
@@ -20,4 +21,11 @@ model = genai.GenerativeModel('gemini-pro')
 txtQUESTION = st.text_input("Digite uma pergunta: ","Escreva um parágrafo sobre Inteligência Artificial e coloque as referências bibliográficas no qual se baseiou.")
 if st.button(label = '✔️ Enviar Pergunta'):
     response = model.generate_content(txtQUESTION)
-    st.write(response.text)
+    resp = response.text
+    st.write(resp)
+    myobj2 = gTTS(text=resp, lang='pt', slow=False)
+    myobj2.save("resp.mp3")   #Saving the converted audio in a mp3 file
+    # Playing the converted file
+    audio_file2 = open('resp.mp3', 'rb')
+    audio_bytes2 = audio_file2.read()
+    st.audio(audio_bytes2, format='audio/ogg',start_time=0)
